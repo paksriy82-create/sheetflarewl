f8cc4a5ea8b538d3c55d7731f0ffbfe748aba25c
paksriy82-create <paksriy82@gmail.com>
2026-03-18 06:33:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,11 @@
 // ==========================================
 
 // 1. Masukkan URL Web App dari Google Apps Script Anda di bawah ini:
-const SCRIPT_URL = "MASUKKAN_URL_WEB_APP_DI_SINI";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyjFzhIulYXd6mFA_fJhIXddnxtNt36iZl97YZV21fG-TiqpZyB4nMtRmMUbw6jJRoI2Q/exec";
 
 // 2. Masukkan ID Spreadsheet Anda di bawah ini:
 // (ID bisa diambil dari link URL Spreadsheet Anda)
-const SHEET_ID = "MASUKKAN_ID_SPREADSHEET_DI_SINI";
+const SHEET_ID = "1GW_Dlg85TsPos-uVUSfKcCoR2tKAQHf3bZoXOJvysQ4";
 
 // ==========================================
 // WAJIB ADA: Jangan ubah kode di bawah ini
